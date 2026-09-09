f66da94b81314d7c3f27284e24417f07469cd7c9
Katarzyna Ziobro <katarzyna.n.ziobro@gmail.com>
2026-09-09 11:54:41 +0200

    fix styling for css

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import("stylelint").Config} */
 export default {
-  extends: ["stylelint-config-standard"]
-};
+  extends: "stylelint-config-standard",
+  "rules": {
+    "custom-property-pattern": [
+      "^([a-z][a-z0-9]*)(_[a-z0-9]+)*$",
+      {
+        "message": "Expected custom property name to be snake_case"
+      }
+    ]
+  }
+}
